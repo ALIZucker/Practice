82c61510cae9138326d000043dbea45620d3ce13
ALI_Zucker <zabihiali1998@gmail.com>
2024-04-24 01:23:55 +0330

    final

--- keyCode/components/boxData/box-data.js
+++ keyCode/components/boxData/box-data.js
@@ -1,8 +1,12 @@
 const template = document.createElement("template");
 
 template.innerHTML = `
-<link rel="stylesheet" href="box-data.css"/>
+<link rel="stylesheet" href="components/boxData/box-data.css" >
+<article>
+<img />
 <h2>Ali</h2>
+</article>
+
 `;
 
 export class Boxdata extends HTMLElement {
@@ -14,7 +18,11 @@ export class Boxdata extends HTMLElement {
   }
   connectedCallback() {
     const data = this.getAttribute("title-box");
+    const imgsrc = this.getAttribute("img-src");
     const h2elm = this.shadowRoot.querySelector("h2");
+    const imgelm = this.shadowRoot.querySelector("img");
+ 
+    imgelm.src=imgsrc
     h2elm.innerHTML = data;
     h2elm.addEventListener("click", (event) => {
       console.log(event.currentTarget);
